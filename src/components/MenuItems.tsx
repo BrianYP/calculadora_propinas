800d9e0385496a9e92d469cd657bdd2f61ab89d8
Brahyan Yepes Cuartas <129774216+BrianYP@users.noreply.github.com>
2024-07-16 15:53:47 -0500

    Cambio de Hooks a Usereducer

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,19 +1,21 @@
 import type { MenuItem } from "../types"
+import { Dispatch } from "react"
+import type { OrderActions } from "../reducers/orderReducer"
 
-type MenuItemProops = {
+type MenuItemProps = {
     item:  MenuItem
     //le decimos que tipo de dato va a ser addItem
-    addItem: (item: MenuItem) => void
+    dispatch: Dispatch<OrderActions>
 }
 
 //atajo para crear componentes rápidos : rfc
-export default function MenuItems({item, addItem} : MenuItemProops) {
+export default function MenuItems({item, dispatch} : MenuItemProps) {
   return (
     <button
     className="border-2
      border-teal-400 hover:bg-teal-200
      w-full p-3 flex justify-between"
-     onClick={() => addItem(item)}
+     onClick={() => dispatch({type: 'add-item', payload:{item}})}
     >
     <p>{item.name}</p>
     <p className="font-black">${item.price}</p>
--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,13 +1,15 @@
 import { formatCurrency } from "../helpers"
-import { MenuItem, OrderItem } from "../types"
+import { OrderItem } from "../types"
+import { Dispatch } from "react"
+import type { OrderActions } from "../reducers/orderReducer"
 
 //relacionamos para que order sea de orderItem
-type OrderContentsProops = {
+type OrderContentsProps = {
     order: OrderItem[],
-    removeItem: (id: MenuItem['id']) => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function OrderContents({ order, removeItem } : OrderContentsProops) {
+export default function OrderContents({ order, dispatch } : OrderContentsProps) {
   return (
     <div>
         <h2 className="font-black text-4xl">Consumo</h2>
@@ -29,7 +31,7 @@ export default function OrderContents({ order, removeItem } : OrderContentsProop
                     </div>
 
                     <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                    onClick={() => removeItem(item.id)}>
+                    onClick={() => dispatch({type: 'remove-item', payload:{id: item.id}})}>
                         X
                     </button>
                 </div>
